bd26d4ed9c9216d2be01be3098e74be8812c41c6
kibet kilel <amos.rotich@strathmore.edu>
2022-04-01 12:50:51 +0300

    Update books views

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+# we  have enabaled our admin page be able to view the model book and we can inputs to the database om the admin
--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Book
+
+# On the top lines we’ve imported ListView and our Book model. Then we create a BookListView
+# class that specifies the model to use and the template (not created yet).
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
