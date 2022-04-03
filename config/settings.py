d511327b843baf0bd4c0b5c0fa155de915f65ab5
kibet kilel <amos.rotich@strathmore.edu>
2022-04-03 21:19:39 +0300

    Heroku host

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from os import environ
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2_8q0g=vte+ao%to^&a+7+x6%aswrt069!au8%*t&jx0&*a5a='
+# SECRET_KEY = 'django-insecure-2_8q0g=vte+ao%to^&a+7+x6%aswrt069!au8%*t&jx0&*a5a='
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = environ['secret_key']
+# SECURITY WARNING: don't run with debug turned on in production!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
